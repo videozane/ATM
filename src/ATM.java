31cf8fa71648acd9cb7821e6528726978c0840ad
Justin Sun <justinsunyt@gmail.com>
2021-08-31 12:46:33 -0700

    Deleted newDeposit and createID, created withdraw
    sheesh

--- src/ATM.java
+++ src/ATM.java
@@ -5,38 +5,46 @@ public class ATM {
 	private HashMap<String, Double> map;
 	
 	public ATM() {
-		map=new HashMap<>();
+		map = new HashMap<>();
 	}
 	 
-	public String createID(){
-		int ranNum;
-		String output="";
-		Random rn= new Random();
-		
-		for(int i=0;i<10;i++) {
-			ranNum=rn.nextInt(122 - 65 + 1) + 65;
-			output+=(char)ranNum;
-		}
-		return output;
-	}
-	
 	public double getBalance(String ID){
 		return map.get(ID);
 	}
 	
-	public void newDeposit(double amount){
-		String id=createID();
-		map.put(id,amount);
-		System.out.println("Balance of $"+amount+" deposited into account with ID "+id+".");
-	}
-	public void idDeposit(String ID, double amount){
-		if(map.get(ID)!=null){
-			map.replace(ID, map.get(ID)+amount);
-			System.out.println("Balance of $"+amount+" deposited into account with ID "+ID+".");
-		}else {
-			map.put(ID,amount);
-			System.out.println("Balance of $"+amount+" deposited into account with ID "+ID+".");
+	public void deposit(String ID, double amount) {
+		if (amount > 0) {
+			if (map.get(ID)!= null) {
+				map.replace(ID, map.get(ID)+amount);
+				System.out.println("Balance of $"+amount+" deposited into account with ID "+ID+".");
+			} else {
+				map.put(ID,amount);
+				System.out.println("Balance of $"+amount+" deposited into account with ID "+ID+".");
+			}
+		} else {
+			System.out.println("Error: cannot depost negative or zero amount.");
 		}
 	}
-	//use hashmaps to connect value (balance) to id (key).
+
+	public void withdraw(String ID, double amount) {
+		if (map.get(ID) != null) {
+			if (amount <= map.get(ID)) {
+				map.replace(ID, map.get(ID)-amount);
+				System.out.println("Balance of $"+amount+" withdrawn from ID "+ID+".");
+			} else {
+				System.out.println("Error: cannot overwithdraw.");
+			}
+		} else {
+			System.out.println("Error: ID does not exist.");
+		}
+	}
+	
+	public void checkBalance(String ID) {
+		if (map.get(ID) != null) {
+			double balance = map.get(ID);
+			System.out.println("Balance: $"+balance+"");
+		} else {
+			System.out.println("Error: ID does not exist.");
+		}
+	}
 }
